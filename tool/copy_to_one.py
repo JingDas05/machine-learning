32f90f050ce674125f5d6a592e166012ce804b76
wusi <wusi@dianwoba.com>
2018-11-28 15:52:43 +0800

    添加拷贝文件小工具

--- tool/copy_to_one.py
+++ tool/copy_to_one.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 import os
 import shutil
+import sys
 
 
 class Copy(object):
@@ -8,21 +9,18 @@ class Copy(object):
     将源文件的所有文件都拷贝到目标文件夹
     Parameters
     ------------
-    source_file_path : string 源文件文件夹
-
     target_file_path : string 目标文件夹
 
     """
 
-    def __init__(self, source_file_path, target_file_path):
-        if source_file_path == "" or target_file_path == "":
+    def __init__(self, target_file_path):
+        if target_file_path == "":
             return
-        self.source_file_path = source_file_path
         self.target_file_path = target_file_path
         # 创建目标文件夹
         os.mkdir(self.target_file_path)
 
-    # 递归 将文件夹下面的文件全部拷贝到目标文件夹里面
+    # 递归 将文件夹下面的文件全部拷贝到目标文件夹里面  initial_path : string 源文件文件夹
     def recursion_copy(self, initial_path):
         file_names = os.listdir(initial_path)
         if len(file_names) == 0:
@@ -36,3 +34,13 @@ class Copy(object):
                 if os.path.exists(target_file_path):
                     continue
                 shutil.copyfile(file_name_path, target_file_path)
+
+
+# if __name__ == "__name__":
+#     print("开始2")
+#     if len(sys.argv) == 3:
+#         print("开始")
+#         copy = Copy(sys.argv[2])
+#         copy.recursion_copy(sys.argv[1])
+#     else:
+#         raise Exception("请输入正确的源文件和目标文件")
